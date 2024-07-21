163775d47e2db0ad331754221e98b3122b0b87fc
violia ruana <violiaruana@gmail.com>
2024-07-21 13:48:12 +0700

    feat: 🎸 add ui in assessment test (athlete)

--- lib/feats/user/presentation/screens/history.dart
+++ lib/feats/user/presentation/screens/history.dart
@@ -1,10 +1,258 @@
+import 'package:dot_coaching/core/widgets/parent.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Parent(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF5868F1), Color(0xFF83348E)],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50.h,
+            right: 0.w,
+            child: SvgPicture.asset(
+              "assets/images/bg/F5F6FF-bg.svg",
+              width: 53.w,
+              height: 139.h,
+            ),
+          ),
+          Container(
+            width: 345.w,
+            height: 106.h,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF5868F1), Color(0xFF83348E)],
+              ),
+              borderRadius: BorderRadius.circular(50.r),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: 344.w,
+              height: 655.h,
+              decoration: BoxDecoration(
+                  color: Color(0xFFF5F6FF),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                    bottomLeft: Radius.circular(0.0),
+                    bottomRight: Radius.circular(0.0),
+                  )),
+            ),
+          ),
+          Positioned(
+            top: 50.h,
+            left: 40.w,
+            right: 40.w,
+            child: Container(
+              width: 257.w,
+              height: 103.h,
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.r),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.25),
+                    blurRadius: 8,
+                    spreadRadius: 0,
+                    offset: const Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Assesment Test",
+                    style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  Image.asset(
+                    'assets/images/exam/exam-logo.png',
+                    width: 50.w,
+                    height: 70.72.w,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 180.h,
+            left: 22.w,
+            right: 22.w,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "History",
+                          style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 16.w,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: 146.w,
+                          height: 30.h,
+                          child: TextField(
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(8),
+                              hintText: 'Search...',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.r),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFF868FE3),
+                              prefixIcon:
+                                  Icon(Icons.search, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                Container(
+                  width: 300.w,
+                  height: 280.h,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFE6E8F9),
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+                  child: ListView.builder(
+                    itemCount: 5,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 271.w,
+                        height: 56.h,
+                        margin: EdgeInsets.only(bottom: 8.h),
+                        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16.r),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 24.w,
+                              height: 24.h,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF763AD8),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              child: Icon(
+                                Icons.sports_volleyball,
+                                color: Colors.white,
+                                size: 18.sp,
+                              ),
+                            ),
+                            SizedBox(width: 8.w),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Novo Club",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  "Volleyball",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12.sp,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 8.w),
+                            SizedBox(
+                              child: OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                  shape: const StadiumBorder(),
+                                  side: const BorderSide(
+                                      width: 1, color: Color(0xFF575FFF)),
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal:
+                                          12), // Adjust padding as needed
+                                ),
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisSize: MainAxisSize
+                                      .min, // Ensure the Row only takes as much width as needed
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .center, // Center align the content inside the button
+                                  children: [
+                                    Text(
+                                      "Download PDF",
+                                      style: TextStyle(
+                                        color: Color(0xFF575FFF),
+                                        fontSize: 11.sp,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    SizedBox(width: 8.w),
+                                    Icon(
+                                      Icons.picture_as_pdf_rounded,
+                                      color: Color(0xFF575FFF),
+                                      size: 12.sp,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
