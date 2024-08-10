72b6d4af4fbdeb9afe2f7609a332806b32d54476
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-08-10 13:25:35 +0700

    fix: 🐛 maybe fix go route listener

--- lib/utils/helpers/go_router_refresh_stream.dart
+++ lib/utils/helpers/go_router_refresh_stream.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 
+import 'package:dot_coaching/utils/helpers/logger.dart';
 import 'package:flutter/material.dart';
 
 class GoRouterRefreshStream extends ChangeNotifier {
   GoRouterRefreshStream(Stream<dynamic> stream) {
     notifyListeners();
     _subscription = stream.asBroadcastStream().listen(
-          (dynamic _) => notifyListeners(),
-        );
+      (dynamic _) {
+        try {
+          notifyListeners();
+        } catch (e) {
+          log.e("GoRouterRefreshStream: $e");
+        }
+      },
+    );
   }
 
   late final StreamSubscription<dynamic> _subscription;
